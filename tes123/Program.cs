8567d04ef9b23258d43c7d345569eaab69f26a7f
fadilfebriansyah <fadilfebriansyah76@gmail.com>
2021-11-06 07:42:06 +0700

    pertama coba

---
+++ tes123/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tes123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.Write("ANAK AYAM TURUN ");
+            }
+            
+        }
+    }
+}
